62b616c078f158ce467d9b1b5200a3cf4ae182ee
gavin-s-smith <gavin.smith@nottingham.ac.uk>
2020-05-26 09:46:47 +0100

    update

--- svmmcr/SVMMCR.py
+++ svmmcr/SVMMCR.py
@@ -240,7 +240,6 @@ class SVMMCR(object):
             """.format(','.join([str(x+1) for x in vars2permute]), min_cv_loss) # +1 due to R indexing
         r(rcode)
 
-        print(r("mcr_te"))
 
         eps_ref_ts = np.asarray(r("eps_ref_te"))
         mcr = np.asarray(r("mcr_te$permuted$range")) 
