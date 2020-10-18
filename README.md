b5eecca12687551262a5bba7a809d7f19e68c5be
gavin-s-smith <gavin.smith@nottingham.ac.uk>
2020-10-18 16:50:28 +0100

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # svmmcr
+A wrapper for Aaron Fishers MCR R code:
+* https://github.com/aaronjfisher/mcr
+* https://github.com/aaronjfisher/mcr-supplement
+
+
+Used as part of the paper:
+
+*Smith, G., Mansilla, R. and Goulding, J. "Model Class Reliance for Random Forests". 34th Conference on Neural Information Processing Systems (NeurIPS 2020), Vancouver, Canada.*
--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='svmmcr',
       version='0.0.1',
       description='Wrapper for Aaron Fishers MCR R code.',
-      url='http://github.com/anon/svmmcr',
-      author='anon',
-      author_email='anon@anon.com',
+      url='http://github.com/gavin-s-smith/svmmcr',
+      author='Gavin Smith',
+      author_email='gavin.smith@nottingham.ac.uk',
       license='MIT',
       packages=['svmmcr'],
       zip_safe=False)
